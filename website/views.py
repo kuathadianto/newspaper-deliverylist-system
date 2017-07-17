8c20dd4ce4b0a6c2c3c8618a46dab36c74f3f98d
Kuat Hadianto <kuathadianto@live.de>
2017-07-17 13:00:18 +0200

    website app are added to settings.py. Added dummy view & template. URLs are configured.

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {'message': 'Hello World!'}
+    return render(request, 'website/index.html', context)
