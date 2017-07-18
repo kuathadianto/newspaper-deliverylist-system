fd05dcf26a8f7518bd196c2ac136f092aa062721
Kuat Hadianto <kuathadianto@live.de>
2017-07-18 20:29:59 +0200

    Changed ImageField to FileField, so Pillow is no more needed.

--- website/models.py
+++ website/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Bild: speichert die Bilder für die Zusatzinformationen
 class Bild(models.Model):
     titel = models.CharField(max_length=100, unique=True)
-    datei = models.ImageField() # braucht Pillow
+    datei = models.FileField()
 
     def __str__(self):
         return self.titel
